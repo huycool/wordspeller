5698383d94de444169eb4a454e1567c152c6b7b5
huycool <hjvn@hotmail.com>
2021-02-07 15:12:26 -0600

    add voice selection

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function speakText(text, pitch, rate, volume) {
     // stop any speaking in progress
     window.speechSynthesis.cancel();
@@ -7,6 +8,13 @@ function speakText(text, pitch, rate, volume) {
     utterance.pitch = pitch;
     utterance.rate = rate;
     utterance.volume = volume;
+
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for (var i = 0; i < voices.length; i++) {
+        if (voices[i].name === selectedOption) {
+            utterance.voice = voices[i];
+        }
+    }
 
     // speak that utterance
     window.speechSynthesis.speak(utterance);
@@ -44,3 +52,36 @@ window.WordSpeller = function () {
     };
 };
 
+var synth = window.speechSynthesis;
+var voices = [];
+var voiceSelect = document.getElementById("voiceSelect");
+
+function populateVoiceList() {
+    // voices = synth.getVoices().sort(function (a, b) {
+    //     const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+    //     if (aname < bname) return -1;
+    //     else if (aname == bname) return 0;
+    //     else return +1;
+    // });
+    voices = synth.getVoices();
+    var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+    voiceSelect.innerHTML = '';
+    for (i = 0; i < voices.length; i++) {
+        var option = document.createElement('option');
+        option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+        if (voices[i].default) {
+            option.textContent += ' -- DEFAULT';
+        }
+
+        option.setAttribute('data-lang', voices[i].lang);
+        option.setAttribute('data-name', voices[i].name);
+        voiceSelect.appendChild(option);
+    }
+    voiceSelect.selectedIndex = selectedIndex;
+}
+
+populateVoiceList();
+if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+    window.speechSynthesis.onvoiceschanged = populateVoiceList;
+}
